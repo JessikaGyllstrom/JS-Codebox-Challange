c236726eb30f8e8dff587cb4becd70526f5e111d
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-05 17:34:12 +0200

    Return URL with the HTTPS protocol

--- script.js
+++ script.js
@@ -11,4 +11,20 @@ function countOdds(numbers) {
 console.log(countOdds([1,5,2,6,5,3,9,2]));//5
 console.log(countOdds([2,6,2,5,2,8]));//1
 
-// Given is an array chars with different letters. Return the array in reverse order.
+// Given is an array chars with different letters. 
+// Return the array in reverse order.
+function reverseLetters(chars) {
+    chars.reverse();
+    return chars;
+}
+console.log(reverseLetters(['b','d','x','p','c']));//[ 'c', 'p', 'x', 'd', 'b' ]
+
+// A string url is given. It contains a URL with the HTTP protocol. 
+// Return this URL with the HTTPS protocol.
+
+function http2https(url) {
+    url = url.substring(0, 4) + "s" + url.substring(4, url.length);
+    return url;
+}
+console.log(http2https('http://jscodebox.com/'));//https://jscodebox.com/
+console.log(http2https('http://jscodebox.com:80/home?admin=1&test=0'));//https://jscodebox.com:80/home?admin=1&test=0
