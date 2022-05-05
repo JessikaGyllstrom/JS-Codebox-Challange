56e29d63be915dbda05deb2004ae1d23cfc4982a
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-05 22:48:49 +0200

    password check

--- script.js
+++ script.js
@@ -80,10 +80,4 @@ console.log(totalProduct(7, 7));//49
 // The HTML element .bg is given. 
 // Change the background color of this element to 'blue'.
 document.write('<div class="bg" style="width: 100%; height: 100%; color: #FFF; display: flex; align-items: center; justify-content: center;">Bluescreen of death!</div>');
-
-document.getElementsByClassName("bg").style.backgroundColor = "blue";
-
-var elements = document.getElementsByClassName('bg'); // get all elements
-	for(var i = 0; i < elements.length; i++){
-		elements[i].style.backgroundColor = "blue";
-	}
+document.getElementsByClassName("bg")[0].style.backgroundColor="blue";
--- scriptII.js
+++ scriptII.js
@@ -1,4 +1,29 @@
-// The HTML element .bg is given. 
-// Change the background color of this element to 'blue'.
-document.write('<div class="bg" style="width: 100%; height: 100%; color: #FFF; display: flex; align-items: center; justify-content: center;">Bluescreen of death!</div>');
-document.getElementsByClassName("bg")[0].style.backgroundColor="blue";
+// Code a for loop that runs through all numbers from 0-99 
+// and append all numbers divisible by seven into a string. 
+// Return this string.
+function easyLoop() {
+  let result = "";
+  let number = 0;
+  for(let i = 0; i < 99; i++) {
+      if(number % 7 == 0) {
+          result += number;
+      }
+      number ++;
+  }
+  return result;
+}
+console.log(easyLoop());
+
+/* Password security 
+Given are two strings password and password_repeat. Check if the password is secure by the following critera:
+- both passwords must match
+- password must be at least 20 chars */
+function checkPassword(password, password_repeat) {
+  if (password == password_repeat && password.length > 20) {
+      return true;
+  } else {
+      return false;
+  }
+}
+console.log(checkPassword('omvdsse', 'omvdsse')); //false
+console.log(checkPassword('gWb8reHTbuo3nbRp5EL625is', 'gWb8reHTbuo3nbRp5EL625is')); //true
