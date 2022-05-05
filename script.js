41e2808b66da473c619d5dca5ee70dfdf82efb0c
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-05 18:09:20 +0200

    operators

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ function bugFixFinal(string) {
 }
 console.log(bugFixFinal('xxbugYYX')); //xxflowerYYX
 console.log(bugFixFinal('xxbugXbugbugYYy')); //xxflowerYYX
+
+// Given are two variables child1 and child2 with a numeric value 
+// between 0 and 99. 
+// Return true if a child is between 0 and 14 (0 and 14 inclusive). 
+// Return false if both or none in between.
+function littleChild(child1, child2) {
+    if ((child1 >= 0 && child1 <=14) && (child2 >= 0 && child2 <=14)) {
+        return false;
+    }
+    if ((child1 >= 0 && child1 <=14) || (child2 >= 0 && child2 <=14)) {
+        return true;
+    } 
+}
+console.log(littleChild(4, 15));//true
+console.log(littleChild(9, 7));//false
