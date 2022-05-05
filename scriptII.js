77722537ca8fe5cd691ad578d392d2aa9bf7aaba
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-05-05 23:17:42 +0200

    every scnd letter in string

--- scriptII.js
+++ scriptII.js
@@ -27,3 +27,23 @@ function checkPassword(password, password_repeat) {
 }
 console.log(checkPassword('omvdsse', 'omvdsse')); //false
 console.log(checkPassword('gWb8reHTbuo3nbRp5EL625is', 'gWb8reHTbuo3nbRp5EL625is')); //true
+
+// Given is a word word. Run through the word and merge every second letter into a new word.
+function strangeWord(word) {
+  let index = 0;
+  let arr = [];
+  let result = "";
+  for(let i = 0; i < word.length; i ++ ) {
+    //console.log(word[index]);
+    arr[i] = word[index];
+    index += 2;
+  }
+  for(let i = 0; i < arr.length; i ++ ) {
+    if(arr[i]!==undefined) {
+      result += arr[i];
+    }
+  }
+  return result;
+}
+console.log(strangeWord('JSCodebox'));
+console.log(strangeWord('Challenge'));
